7e0f6eedb269098991ac3e8b4267e62a16e31232
Darina Khalina <darina.khalina@namecheap.com>
2024-06-27 22:53:02 +0300

    chore(hw7): add task 4

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,57 @@
-// const key = new Key();
+class Key {
+  private signature: number = Math.random();
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+  public getSignature(): number {
+    return this.signature;
+  }
+}
 
-// house.openDoor(person.getKey());
+class Person {
+  constructor(private key: Key) {}
 
-// house.comeIn(person);
+  public getKey(): Key {
+    return this.key;
+  }
+}
 
-// export {};
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("You have permission to enter the house.");
+    } else {
+      console.log("You do not have permission to enter the house.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+}
+
+const key = new Key();
+// const key2 = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+// const person = new Person(key2);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
